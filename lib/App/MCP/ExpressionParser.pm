8f45daaa88fc15c7aad5b5734625ce2766b99829
pjf <pjf@37e0a6e5-49c4-483a-aa74-46890460ded1>
2012-11-21 17:37:29 +0000

    Awesome

---
+++ lib/App/MCP/ExpressionParser.pm
@@ -0,0 +1,233 @@
+# @(#)$Id$
+
+package App::MCP::ExpressionParser;
+
+use strict;
+use warnings;
+use feature qw(state);
+use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev$ =~ /\d+/gmx );
+
+use CatalystX::Usul::Functions qw(arg_list throw);
+use Marpa::R2;
+
+sub new {
+   my ($self, @args) = @_; my $attr = arg_list @args;
+
+   return bless { external => $attr->{external},
+                  tokens   => __tokens( $attr ), }, ref $self || $self;
+}
+
+sub parse {
+   my ($self, $line) = @_; my $identifiers = {}; my $last = 0;
+
+   my $line_length = length $line; my $pos = 0; my $rec = $self->_recogniser;
+
+   while ($pos < $line_length) {
+      my $expected_tokens = $rec->terminals_expected;
+
+      scalar @{ $expected_tokens } or next;
+
+      my @matching_tokens = $self->_lex( \$line, $pos, $expected_tokens );
+
+      for my $token (@matching_tokens) {
+         $pos += $token->[ 2 ]; $token->[ 0 ] eq 'SP' and next;
+
+         $token->[ 0 ] eq 'IDENTIFIER' and $identifiers->{ $token->[ 1 ] } = 1;
+
+         $rec->read( @{ $token } );
+      }
+
+      $pos == $last
+         and throw error => 'Expression [_1] parse error - byte [_2]',
+                   args  => [ $line, $pos ];
+      $last = $pos;
+   }
+
+   $rec->end_input; my $value_ref = $rec->value;
+
+   $value_ref or throw error => 'Expression [_1] does not parse',
+                       args  => [ $line ];
+
+   return [ ${ $value_ref }, [ keys %{ $identifiers } ] ];
+}
+
+# Private methods
+
+sub _grammar {
+   my $self = shift; state $cache; $cache and return $cache;
+
+   my $grammar = Marpa::R2::Grammar->new( {
+      action_object   => 'external',
+      actions         => 'App::MCP::ExpressionParser::Actions',
+      start           => 'expression',
+      rules           => [
+         [ expression => [ qw(expression OPERATOR expression) ] => 'operate'  ],
+         [ expression => [ qw(LP expression RP)               ] => 'subrule2' ],
+         [ expression => [ qw(NOT expression)                 ] => 'negate'   ],
+         [ expression => [ qw(call)                           ] => 'subrule1' ],
+         [ call       => [ qw(function LP job_name RP)        ] => 'callfunc' ],
+         [ function   => [ 'PREDICATE'                        ] => 'subrule1' ],
+         [ job_name   => [ 'IDENTIFIER'                       ] => 'subrule1' ],
+         ],
+      } );
+
+   $grammar->precompute;
+
+   return $cache = $grammar;
+}
+
+sub _lex {
+   my ($self, $input, $pos, $expected) = @_; my @matches;
+
+ TOKEN: for my $token_name ('SP', @{ $expected }) {
+      my $token = $self->{tokens}->{ $token_name };
+
+      defined $token
+         or throw error => 'Token [_1] unknown', args => [ $token_name ];
+
+      my $rule = $token->[ 0 ];
+
+      pos( ${ $input } ) = $pos; ${ $input } =~ $rule or next TOKEN;
+
+      my $matched_len = $+[ 0 ] - $-[ 0 ]; my $matched_value = undef;
+
+      if (defined( my $val = $token->[ 1 ] )) {
+         if (ref $val eq 'CODE') { $matched_value = $val->() }
+         else { $matched_value = $val }
+      }
+      elsif ($#- > 0) { # Captured a value
+         $matched_value = $1;
+      }
+
+      push @matches, [ $token_name, $matched_value, $matched_len ];
+   }
+
+   return @matches;
+}
+
+sub _recogniser {
+   my $self = shift;
+
+   return Marpa::R2::Recognizer->new( {
+      closures        => { 'external::new' => sub { $self->{external} } },
+      grammar         => $self->_grammar,
+      ranking_method  => 'rule',
+     trace_terminals => 2,
+     trace_values    => 1,
+     trace_actions   => 1,
+   } );
+}
+
+# Private functions
+
+sub __tokens {
+   my $attr = shift; my $predicates = join '|', @{ $attr->{predicates} };
+
+   return {
+      'LP'         => [ qr{ \G [\(]                }msx      ],
+      'RP'         => [ qr{ \G [\)]                }msx      ],
+      'SP'         => [ qr{ \G [ ]                 }msx, ' ' ],
+      'NOT'        => [ qr{ \G [\!]                }msx      ],
+      'OPERATOR'   => [ qr{ \G (\&|\|)             }msx      ],
+      'PREDICATE'  => [ qr{ \G ($predicates)       }msx      ],
+      'IDENTIFIER' => [ qr{ \G ([a-zA-Z0-9_\-\+]+) }msx      ],
+   };
+}
+
+package # Hide from indexer
+   App::MCP::ExpressionParser::Actions;
+
+sub callfunc {
+   my $func = $_[ 1 ]; return $_[ 0 ]->$func( $_[ 3 ] );
+}
+
+sub negate {
+   return $_[ 2 ] ? 0 : 1;
+}
+
+sub operate {
+   return $_[ 2 ] eq '&' ? $_[ 1 ] && $_[ 3 ] : $_[ 1 ] || $_[ 3 ];
+}
+
+sub subrule1 {
+   return $_[ 1 ];
+}
+
+sub subrule2 {
+   return $_[ 2 ];
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 Name
+
+App::MCP::ExpressionParser - <One-line description of module's purpose>
+
+=head1 Version
+
+0.1.$Revision$
+
+=head1 Synopsis
+
+   use App::MCP::ExpressionParser;
+   # Brief but working code examples
+
+=head1 Description
+
+=head1 Configuration and Environment
+
+=head1 Subroutines/Methods
+
+=head2 new
+
+=head2 parse
+
+=head1 Diagnostics
+
+=head1 Dependencies
+
+=over 3
+
+=item L<Class::Usul>
+
+=back
+
+=head1 Incompatibilities
+
+There are no known incompatibilities in this module
+
+=head1 Bugs and Limitations
+
+There are no known bugs in this module.
+Please report problems to the address below.
+Patches are welcome
+
+=head1 Acknowledgements
+
+Larry Wall - For the Perl programming language
+
+=head1 Author
+
+Peter Flanigan, C<< <Support at RoxSoft dot co dot uk> >>
+
+=head1 License and Copyright
+
+Copyright (c) 2012 Peter Flanigan. All rights reserved
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself. See L<perlartistic>
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+
+=cut
+
+# Local Variables:
+# mode: perl
+# tab-width: 3
+# End:
